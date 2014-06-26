6a2108199fa2d9f61f0954b34a22c59e3cf277ec
unknown <mattia.cappa@Satellite315.gruppoblueteam.lan>
2014-06-26 17:47:15 +0200

    Refactoring

---
+++ src/yahtzee/Category.java
@@ -0,0 +1,13 @@
+package yahtzee;
+
+public class Category {
+	private String category;
+
+	public void setCategory(String choosenCategory) {
+		category = choosenCategory;		
+	}
+	
+	public String getCategory() {
+		return category;		
+	}
+}
