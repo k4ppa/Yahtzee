4e13ffc576e4fa294e3fcf3cd10b63845b6dd4e7
unknown <mattia.cappa@Satellite315.gruppoblueteam.lan>
2014-06-30 17:06:57 +0200

    Refactoring

---
+++ src/rules/ThreeAndFourOfAKindRule.java
@@ -0,0 +1,19 @@
+package rules;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class ThreeAndFourOfAKindRule implements YahtzeeRule {
+
+	@Override
+	public Integer ruleScore(Map<Integer, Integer> diceMap) {
+		Integer score = new Integer(0);
+		
+		for (Iterator<?> nextDie = diceMap.keySet().iterator(); nextDie.hasNext();) {
+			Integer die = (Integer) nextDie.next();
+			score += die * diceMap.get(die);
+		}
+		return score;
+	}
+
+}
